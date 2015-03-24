7e5f3639bd0bf152cb58a3066c3d74aefb47675c
Lincoln Samelson <lincoln.samelson@colorado.edu>
2015-03-24 15:59:27 -0700

    Fixed improper push.

---
+++ app/src/main/java/com/dev/foundingfourfathers/alchemy/PossibleDrinks.java
@@ -0,0 +1,20 @@
+package com.dev.foundingfourfathers.alchemy;
+
+import java.util.Locale;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+
+public class PossibleDrinks extends ActionBarActivity{
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_possible_drinks);
+
+
+    }
+}
