f1f8fd4c6f3313d0136a768a0a4bcb65a2e9f79a
Lincoln Samelson <lincoln.samelson@colorado.edu>
2015-04-12 22:32:04 -0600

    Added Spinner.  BasketPage doesn't work because it is extending ActionBarActivity instead of Activity.

--- app/src/main/java/com/dev/foundingfourfathers/alchemy/HomePage.java
+++ app/src/main/java/com/dev/foundingfourfathers/alchemy/HomePage.java
@@ -1,5 +1,6 @@
 package com.dev.foundingfourfathers.alchemy;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -13,12 +14,13 @@ import android.widget.Toast;
 import java.util.List;
 
 
-public class HomePage extends ActionBarActivity {
+public class HomePage extends Activity {
 
     AlchemyDB database;
     private Button b_toBasket;
     private Button dbTest;
     private Button b_make_drink;
+    private Button b_to_Spinner;
     ImageView logo;
 
 
@@ -41,6 +43,15 @@ public class HomePage extends ActionBarActivity {
             @Override
             public void onClick(View view) {
                 Intent myIntent = new Intent(view.getContext(), BasketPage.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
+
+        b_to_Spinner = (Button) findViewById(R.id.b_to_Spinner);
+        b_to_Spinner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), SpinnerTest.class);
                 startActivityForResult(myIntent, 0);
             }
         });
