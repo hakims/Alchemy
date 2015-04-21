d811f42e50328556955813cef795a4e33ce5e5a7
Lincoln Samelson <lincoln.samelson@colorado.edu>
2015-04-20 21:29:15 -0600

    Updated Drinkmenu to have light and hard alcohols.

--- app/src/main/java/com/dev/foundingfourfathers/alchemy/DrinkMaker.java
+++ app/src/main/java/com/dev/foundingfourfathers/alchemy/DrinkMaker.java
@@ -1,9 +1,15 @@
 package com.dev.foundingfourfathers.alchemy;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.support.v4.app.NavUtils;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -14,7 +20,7 @@ public class DrinkMaker extends Activity {
      * is a {@link android.widget.LinearLayout}.
      */
 
-
+    private ViewGroup mContainerView;
 
 
 
@@ -34,9 +40,16 @@ public class DrinkMaker extends Activity {
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle presses on the action bar items
         switch (item.getItemId()) {
-            case R.id.action_add_item:
-                Toast.makeText(getApplicationContext(), "HELLO!", Toast.LENGTH_SHORT).show();
+            case android.R.id.home:
+                // Navigate "up" the demo structure to the launchpad activity.
+                // See http://developer.android.com/design/patterns/navigation.html for more.
+                NavUtils.navigateUpTo(this, new Intent(this, HomePage.class));
                 return true;
+
+            case R.id.Alcohol:
+
+                Toast.makeText(getApplicationContext(), "Alcohol!!", Toast.LENGTH_SHORT).show();
+                //addItem();
 
             default:
                 return super.onOptionsItemSelected(item);
@@ -46,14 +59,15 @@ public class DrinkMaker extends Activity {
 
     private void addItem() {
 
-        /*
+
         // Instantiate a new "row" view.
         final ViewGroup newView = (ViewGroup) LayoutInflater.from(this).inflate(
                 R.layout.list_item_example, mContainerView, false);
 
         // Set the text in the new row to a random country.
-        ((TextView) newView.findViewById(android.R.id.text1)).setText(
-                COUNTRIES[(int) (Math.random() * COUNTRIES.length)]);
+        ((TextView) newView.findViewById(android.R.id.text1)).setText("Gin!"
+
+        );
 
         // Set a click listener for the "X" button in the row that will remove the row.
         newView.findViewById(R.id.delete_button).setOnClickListener(new View.OnClickListener() {
@@ -74,7 +88,7 @@ public class DrinkMaker extends Activity {
         // Because mContainerView has android:animateLayoutChanges set to true,
         // adding this view is automatically animated.
         mContainerView.addView(newView, 0);
-        */
+
     }
 
 
